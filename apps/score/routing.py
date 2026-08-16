f04948cb492b3229fbd9e9de76bbfcb1d577a12f
Mamungithube <orroshidmdmamun50@gmail.com>
2026-08-16 10:10:25 +0600

    feat: add websocket routing for live score updates

--- apps/score/routing.py
+++ apps/score/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from apps.score import consumers
 
 websocket_urlpatterns = [
-    re_path(r'^ws/scores/live/$', consumers.LiveScoreConsumer.as_asgi()),
+    re_path(r'^ws/scores/live/?$', consumers.LiveScoreConsumer.as_asgi()),
 ]
